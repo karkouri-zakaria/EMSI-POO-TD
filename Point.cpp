2b2f7e42e33d68dfa41891bb743936342cc694a9
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2021-11-12 23:26:37 +0100

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -14,12 +14,24 @@ Point::Point(double x, double y)
 	this->y = y;
 }
 
-void Point::Afficher()
+void Point::Afficher() const
 {
 	cout << "(" << this->x << "," << this->y << ")";
 }
 
-double Point::Distance(Point z2)
+double Point::Distance(const Point &z2)const
 {
 	return sqrt(pow(this->x - z2.x, 2) + pow(this->y - z2.y, 2));
+}
+
+void Point::translate(double dx, double dy)
+{
+	this->x = this->x + dx;
+	this->y = this->y + dy;
+}
+
+Point Point::Origine()
+{
+	Point p;
+	return(p);
 }
