d6c2c09589363c9ae9dd34d5868ea55cd168f394
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2021-12-23 07:20:07 +0100

    Update Structures.cpp

--- Structures.cpp
+++ Structures.cpp
@@ -1,5 +1,5 @@
 // Structures.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Pile type double
 
 #include <iostream>
 #include"Pile.h"
@@ -14,7 +14,7 @@ int main()
 
 	P1.AfficherPile();
 	std::cout << P1.Pilepleine() << endl;
-	P1.Dépiler();
+	P1.DÃ©piler();
 	P1.AfficherPile();
 
 	std::cout << P1.Pilevide() << endl;
@@ -28,14 +28,3 @@ int main()
 	P1.ChangerTaille(5);
 	P1.AfficherPile();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
