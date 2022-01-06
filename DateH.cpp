7bca540a13e9b43c629e1b238f6d2a8d6f057b7d
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-01-06 17:50:11 +0100

    Update DateH.cpp

--- DateH.cpp
+++ DateH.cpp
@@ -9,12 +9,12 @@ DateH::DateH(int h = 0, int min = 0, int sec = 0, int D = 1, int M = 1, int Y = 
 	this->D = D;
 	this->M = M;
 	this->Y = Y;
-	if (!(0 < h && h < 24)) { this->h = 0;}
-	if (!(0 < min && min < 60)) { this->min = 0; };
-	if (!(0 < sec && sec < 60)) { this->sec = 0; };
-	if (!(0 < D && D < 32)) { this->D = 1; };
-	if (!(0 < M && M < 13)) { this->M = 1; };
-	if (!(1899 < Y && Y < 2501)) { this->Y = 1900; };
+	if (0 > h && h > 24) { this->h = 0;}
+	if (0 > min && min > 60) { this->min = 0; };
+	if (0 > sec && sec > 60) { this->sec = 0; };
+	if (0 > D && D > 32) { this->D = 1; };
+	if (0 > M && M > 13) { this->M = 1; };
+	if (1899 > Y && Y > 2501) { this->Y = 1900; };
 }
 
 void DateH::affiche() const
