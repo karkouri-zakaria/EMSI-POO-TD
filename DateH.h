1bc45052e5cbc21dabf166d97bb1e1ea01442f09
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-01-06 15:06:53 +0100

    Update DateH.h

--- DateH.h
+++ DateH.h
@@ -6,7 +6,7 @@ class DateH : public Heure
 private:
 	int D, M, Y;
 public:
-	DateH(int,int,int,int,int,int);
+	DateH(int h=0,int min=0,int sec=0,int D=1,int M=1,int Y=1900);
 	void Show()const;
 	bool operator>(const DateH&)const;
 
