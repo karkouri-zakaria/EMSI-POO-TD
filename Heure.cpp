8015af652df9bdc200cb35cbd93da92a6bd9031b
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-01-06 17:48:25 +0100

    Update Heure.cpp

--- Heure.cpp
+++ Heure.cpp
@@ -7,9 +7,9 @@ Heure::Heure(int h=0, int min=0, int sec=0)
 	this->h = h;
 	this->min = min;
 	this->sec = sec;
-	if (!(0 < h && h < 24)) { this->h = 0; }
-	if (!(0 < min && min < 60)) { this->min = 0; };
-	if (!(0 < sec && sec < 60)) { this->sec = 0; };
+	if (0 > h && h > 24) { this->h = 0; }
+	if (0 > min && min > 60) { this->min = 0; };
+	if (0 > sec && sec > 60) { this->sec = 0; };
 }
 
 void Heure::affiche() const
@@ -24,11 +24,12 @@ void Heure::affiche() const
 
 bool Heure::operator>(const Heure& H)const
 {
-	if (this->h <= H.h) {
-		if (this->min <= H.min) {
-			if (this->sec <= H.sec) {
-				return false;
-			}
-		}
-	} return true;
+	return(this->convertTOsec() > Dt.convertTOsec());
 }
+
+int Heure::convertTOsec() const
+{
+	int res = this->h * 3600 + this->min * 60 + this->sec;
+	return res;
+}
+
