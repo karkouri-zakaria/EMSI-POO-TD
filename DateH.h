389dae216017346557970f5ff046271a150bd8db
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-01-06 15:07:16 +0100

    Update DateH.h

--- DateH.h
+++ DateH.h
@@ -7,7 +7,7 @@ private:
 	int D, M, Y;
 public:
 	DateH(int h=0,int min=0,int sec=0,int D=1,int M=1,int Y=1900);
-	void Show()const;
+	void affiche()const;
 	bool operator>(const DateH&)const;
 
 };
